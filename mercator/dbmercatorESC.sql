511d56ded680804469d474d801e9cd4b86c631f8
miicksgarza <no_8995@hotmail.com>
2016-06-13 12:33:07 -0500

    Se agrego la carpeta para agregar el script de la base de datos

---
+++ mercator/dbmercatorESC.sql
@@ -0,0 +1,9 @@
+﻿/*
+NOMBRE SRCIPT:  dbSaficMTY
+DESCRIPTION : script to creation of database
+VERSION : 1.0
+HISTORIA DE MODIFICACION: IS    -    FECHA    -     Descripcion cambio
+						  ALR		13/06/16		Cration script of database db
+*/
+USE MASTER 
+GO
